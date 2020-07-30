46f6d24eb08541141b1d3ff6efd0bcdb442c6d5a
Riccardo Zese <riccardo.zese@unife.it>
2020-07-30 11:47:05 +0200

    Added examples

---
+++ prolog/examples/nixon.pl
@@ -0,0 +1,32 @@
+:-use_module(library(trill)).
+
+:- trill. % or :- trillp. or :- tornado.
+
+/*
+An inconsistent KB representing the famous example from the literature on non-monotonic reasoning Nixon Example.
+*/
+
+/** <examples>
+
+?- prob_instanceOf(complementOf(pacifist),nixon,Prob).
+?- instanceOf(complementOf(pacifist),nixon,ListExpl).
+
+*/
+
+subClassOf(quaker,pacifist).
+subClassOf(republican,complementOf(pacifist)).
+subClassOf(president,republican).
+subClassOf(president,quaker).
+
+subClassOf(regular,president).
+subClassOf(impeachment,complementOf(president)).
+classAssertion(impeachment,nixon).
+classAssertion(regular,nixon). 
+
+
+annotationAssertion('disponte:probability',subClassOf(quaker,pacifist),literal('0.98')).
+annotationAssertion('disponte:probability',subClassOf(republican,complementOf(pacifist)),literal('0.7')).
+annotationAssertion('disponte:probability',subClassOf(president,republican),literal('0.5')).
+annotationAssertion('disponte:probability',subClassOf(president,quaker),literal('0.5')).
+annotationAssertion('disponte:probability',subClassOf(regular,president),literal('1.0')).
+annotationAssertion('disponte:probability',subClassOf(impeachment,complementOf(president)),literal('0.9')).
---
+++ prolog/examples/pingu.pl
@@ -0,0 +1,23 @@
+:-use_module(library(trill)).
+
+:- trill. % or :- trillp. or :- tornado.
+
+/*
+An inconsistent KB representing the classical examples about flying penguins.
+*/
+
+/** <examples>
+
+?- prob_instanceOf(fly,pingu,Prob).
+?- instanceOf(fly,pingu,ListExpl).
+
+*/
+
+subClassOf(bird,fly).
+subClassOf(penguin,bird).
+subClassOf(penguin,complementOf(fly)).
+classAssertion(penguin,pingu).
+
+annotationAssertion('disponte:probability',subClassOf(bird,fly),literal('0.999999999')).
+%annotationAssertion('disponte:probability',subClassOf(penguin,complementOf(fly)),literal('0.9')).
+ 
