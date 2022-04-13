b687e93935c9292ecc4e142be0a15b3c9890b127
Riccardo Zese <riccardo.zese@unife.it>
2022-04-13 10:17:03 +0200

    updated example for trans and sym property

--- prolog/examples/transitive_property.pl
+++ prolog/examples/transitive_property.pl
@@ -8,5 +8,5 @@ classAssertion( smartphone, sm0815).
 propertyAssertion( tp, a, b).
 propertyAssertion( tp, b, c).
 transitiveProperty(tp).
-%symmetricProperty(tp).
-%inverseProperties(tp,tpi).
+symmetricProperty(tp).
+inverseProperties(tp,tpi).
