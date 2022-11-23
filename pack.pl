9c7324f28c7d07e16a4389b144148350e7afb3a3
Riccardo Zese <riccardo.zese@unife.it>
2022-11-23 16:41:26 +0100

    fix minor bug in clashes

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(trill).
 title('A tableau probabilistic reasoner in three different versions').
-version('6.0.5').
+version('6.0.6').
 author('Riccardo Zese', 'zsercr@unife.it').
 download('https://github.com/rzese/trill/releases/*.zip').
 requires(bddem).
--- prolog/trill.pl
+++ prolog/trill.pl
@@ -990,10 +990,10 @@ clash(M,C-Ind,Tab,Expl):-
 clash(M,sameIndividual(LS),Tab,Expl):-
   get_abox(Tab,ABox),
   %write('clash 2.a'),nl,
-  find((sameIndividual(LS),Expl1),ABox),
+  findSameIndividual(LS,(sameIndividual(LSABox),Expl1),ABox),
   find((differentIndividuals(LD),Expl2),ABox),
-  member(X,LS),
-  member(Y,LS),
+  member(X,LSABox),
+  member(Y,LSABox),
   member(X,LD),
   member(Y,LD),
   dif(X,Y),
@@ -1002,7 +1002,7 @@ clash(M,sameIndividual(LS),Tab,Expl):-
 clash(M,differentIndividuals(LS),Tab,Expl):-
   get_abox(Tab,ABox),
   %write('clash 2.b'),nl,
-  find((differentIndividuals(LS),Expl1),ABox),
+  findDifferentIndividuals(LS,(differentIndividuals(LSABox),Expl1),ABox),
   find((sameIndividual(LD),Expl2),ABox),
   member(X,LS),
   member(Y,LS),
@@ -1104,6 +1104,22 @@ make_expl(M,Ind,S,[H|T],Expl0,ABox,Expl):-
   and_f(M,Expl2,Expl0,Expl1),
   make_expl(M,Ind,S,T,Expl1,ABox,Expl).
 */
+
+findSameIndividual(LS,(sameIndividual(LSABox),Expl),ABox):-
+  find((sameIndividual(LSABox),Expl),ABox),
+  all_members(LS,LSABox).
+
+findDifferentIndividuals(LS,(differentIndividuals(LSABox),Expl),ABox):-
+  find((differentIndividuals(LSABox),Expl),ABox),
+  all_members(LS,LSABox).
+
+all_members(LS,LSABox):-
+  member(H1,LS),
+  member(H2,LS),
+  dif(H1,H2),
+  member(H1,LSABox),
+  member(H2,LSABox),!.
+
 
 
 :- multifile check_clash/3.
